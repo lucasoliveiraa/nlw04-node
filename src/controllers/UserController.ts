755fdd45f9fa67cb9279f4d8d754e2982ab41a43
lupandemo <lucasjackcosta18@gmail.com>
2021-02-28 00:52:15 -0300

    CreateSurveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,29 +1,30 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
-    const userRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    const userAlreadyExists = await userRepository.findOne({
-      email
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
     });
 
+    // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
     if(userAlreadyExists) {
       return response.status(400).json({
         error: "User already exists!",
       });
     }
 
-    const user = userRepository.create({
+    const user = usersRepository.create({
       name,
       email
     });
 
-    await userRepository.save(user);
+    await usersRepository.save(user);
 
     return response.status(201).json(user);
   }
